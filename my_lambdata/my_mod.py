26d14e051700262aa1f92fdb0e8f06113bf51a18
ChristopherMajor <MajorGlassLabs@Gmail.com>
2020-04-06 18:05:44 -0700

    import function from another file

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -6,6 +6,11 @@ def enlarge(n):
     return n*100
 
 
-x=int(input("Choose a number."))
-result = enlarge(x)
-print(result)
+## x=int(input("Choose a number."))
+## result = enlarge(x)
+## print(result)
+
+if __name__ == "__main__":
+    x=int(input("Choose a number."))
+    result = enlarge(x)
+    print(result)
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,7 +1,12 @@
 
 import pandas
 
+from my_lambdata.my_mod import enlarge
+
+
 print('Hello world uWu')
 
 df = pandas.DataFrame({"state": ['CT', 'CO', 'CA', 'TX']})
 print(df.head())
+
+print(enlarge(9))
